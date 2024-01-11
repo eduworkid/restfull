5480df5ebd88e10a85e78a964855b3f0b5ecc525
rizkifadilah12 <rf8740072@gmail.com>
2024-01-11 18:39:54 +0700

    first commit

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import {Sequelize} from "sequelize";
 
-const db = new Sequelize('upload_db','root','',{
-    host: 'localhost',
-    dialect: "mysql"
+const db = new Sequelize("edapi", "root", "", {
+  host: "localhost",
+  dialect: "mysql",
 });
 
 export default db;
